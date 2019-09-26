92f9140cae562faac9d1afbce5a9cd3447d48880
David <davidsimonkastle@gmail.com>
2019-09-26 01:51:08 -0300

    datasets and jsons

--- hdf5_to_json.py
+++ hdf5_to_json.py
@@ -2,19 +2,17 @@ import h5py
 import json
 import io
 
-
 filename = 'sensor_records.hdf5'
-treePath = 'trajectory_0000/gps/position'
+treePath = 'trajectory_0000/imu/gyroscope'
 
 f = h5py.File(filename, 'r')
-
 
 # Get the data
 data = f[treePath]
 
 data = data[()].tolist()
 
-with io.open('data.json', 'w', encoding='utf8') as outfile:
+with io.open('gyroscope_data.json', 'w', encoding='utf8') as outfile:
     str_ = json.dumps(data,
                       indent=4, sort_keys=True,
                       separators=(',', ': '), ensure_ascii=False)
--- main.js
+++ main.js
@@ -1,23 +1,102 @@
-var awsIot = require("aws-iot-device-sdk");
+const awsIot = require("aws-iot-device-sdk");
+const crypto = require("crypto");
+const fs = require("fs");
 
-var device = awsIot.device({
+const device = awsIot.device({
   keyPath: "./auth/drone-sim-0.private.key",
   certPath: "./auth/drone-sim-0.cert.pem",
   caPath: "./auth/root-CA.crt",
-  clientId: "sdk-nodejs-53c03dd9-b543-40ca-879e-a4eb0ddbd3e0",
+  // clientId: "sdk-nodejs-53c03dd9-b543-40ca-879e-a4eb0ddbd3e0",
+  clientId: "sdk-nodejs-DR-01",
   host: "ap60jigczfevx-ats.iot.us-east-2.amazonaws.com"
 });
 
-//
-// Device is an instance returned by mqtt.Client(), see mqtt.js for full
-// documentation.
-//
+// IoT Fake Data
+const velocity = JSON.parse(fs.readFileSync("velocity_data.json", "utf8"));
+const position = JSON.parse(fs.readFileSync("position_data.json", "utf8"));
+const accelerometer = JSON.parse(
+  fs.readFileSync("accelerometer_data.json", "utf8")
+);
+
+let battery = 100;
+
+let stance = "moving";
+
+const serialNumber = "DR-01";
+
+let currentTime = 0;
+
+function setStance(value) {
+  stance = value;
+}
+
+function batteryConsumption() {
+  if (stance === "idle") {
+    battery -= 0.5;
+  } else {
+    battery -= 1;
+  }
+}
+
+function registerDevice() {
+  const payload = {
+    serialNumber
+  };
+
+  device.publish("drone_network/registration", JSON.stringify(payload));
+}
+
+function sendMonitoringData() {
+  const payload = {
+    serialNumber,
+    velocity: velocity[currentTime],
+    position: position[currentTime],
+    //attitude: attitude[currentTime],
+    accelerometer: accelerometer[currentTime],
+    battery,
+    stance,
+    time: new Date()
+  };
+
+  if (stance !== "idle") {
+    currentTime += 1;
+  }
+
+  device.publish(`drone_network/monitor`, JSON.stringify(payload));
+}
+
+let batteryInterval;
+let monitoringDataInterval;
+
 device.on("connect", function() {
-  console.log("connect");
-  device.subscribe("topic_1");
-  // device.publish("topic_2", JSON.stringify({ test_data: 1 }));
+  console.log("IoT Dummy Drone Connected.");
+
+  batteryInterval = setInterval(batteryConsumption, 10000);
+  monitoringDataInterval = setInterval(sendMonitoringData, 2500);
+
+  registerDevice();
+
+  device.subscribe("drone_network/controller");
+});
+
+device.on("error", function(error) {
+  console.log("error", error);
 });
 
 device.on("message", function(topic, payload) {
   console.log("message", topic, payload.toString());
+
+  const parsedPayload = JSON.parse(payload);
+  if (parsedPayload.target === serialNumber) {
+    switch (parsedPayload.order) {
+      case "stop":
+        setStance("idle");
+        break;
+      case "move":
+        setStance("moving");
+        break;
+      default:
+        break;
+    }
+  }
 });
